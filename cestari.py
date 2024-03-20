33d210ea2344ce37379053acc32901c257aa1974
stefano.cestari <stafno.cestari2203@gmail.com>
2024-03-20 13:29:24 +0100

    esercizio stfano cestari aggironato ancora pssword manager del prof non utilizzabile

--- cestari.py
+++ cestari.py
@@ -1,64 +1,46 @@
-# importare i moduli crittografici
-from Crypto.Protocol.KDF import scrypt
-from Crypto.Cipher import ChaCha20_Poly1305
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
 from Crypto.Hash import BLAKE2b
+from Crypto.Protocol.KDF import scrypt
+from Crypto.Random import get_random_bytes
+from getpass import getpass
 import json
 import os.path
-import getpass
 
 def process_pwd(password, salt):
-    # elabora la password in maniera opportuna
-    return scrypt(password, salt, 32, N=2**14, r=8, p=1)
+    # Use the password and scrypt to generate a key
+    key = scrypt(password, salt, key_len=32, N=2**14, r=8, p=1)
+    return key
 
 def load_data(path, password):
-    credentials = dict()
-    try:
-        with open(path, 'rb') as data:
-            print("Decrypting file...")
-            salt = data.read(16)
-            nonce = data.read(12)  # Correzione: leggiamo un nonce di 12 byte
-            tag = data.read(16)
-            ciphertext = data.read()
-            # key generator
-            key = process_pwd(password, salt)
-            # Decrypt with ChaCha20
-            cipher = ChaCha20_Poly1305.new(key=key, nonce=nonce)
-            credentials = cipher.decrypt_and_verify(ciphertext, tag)
-    except Exception as e:
-        print(str(e))
-        return None
-    return credentials
+    with open(path, 'rb') as in_file:
+        salt = in_file.read(16)
+        nonce = in_file.read(15)
+        tag = in_file.read(16)
+        ciphertext = in_file.read(-1)
 
+    key = process_pwd(password, salt)
+    cipher = AES.new(key, AES.MODE_OCB, nonce=nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
+    try: 
+        credentials = json.loads(data.decode('utf-8'))
+    except ValueError as err:
+        raise IOError(f'data not valid: {str(err)}')
+    return credentials
 
 def save_and_exit(path, password, credentials):
     data = json.dumps(credentials, ensure_ascii=False).encode('utf-8')
-    print("Encrypting file...")
-    # Generate the salt
     salt = get_random_bytes(16)
-    # Generate a nonce of correct length
-    nonce = get_random_bytes(12)  # Correzione: generiamo un nonce di 12 byte
-    # key generator
     key = process_pwd(password, salt)
-    # ChaCha20-Poly1305
-    cipher = ChaCha20_Poly1305.new(key=key, nonce=nonce)
+    cipher = AES.new(key, AES.MODE_OCB)
     ciphertext, tag = cipher.encrypt_and_digest(data)
-    
     with open(path, 'wb') as out_file:
         out_file.write(salt)
-        out_file.write(nonce)
+        out_file.write(cipher.nonce)
         out_file.write(tag)
         out_file.write(ciphertext)
-        
 
 def search_and_add(query, dic):
-    hasher = BLAKE2b.new(digest_bits=256)  # Usiamo BLAKE2b con una lunghezza di digest di 256 bit
-    hasher.update(query.encode())  # Aggiorna l'hasher con i byte della query
-    query_hash = hasher.digest() 
-    query_hash_hex = query_hash.hex()    # Converti il digest hash in una stringa 
-    
-    if query_hash in dic:
+    if query in dic:
         print('username: ', dic[query]['username'])
         print('password: ', dic[query]['password'])
     else:
@@ -67,8 +49,7 @@ def search_and_add(query, dic):
         add = input(prompt)
         if add == 'y':
             username_n = input('Insert username: ')
-            # leggi la password in maniera opportuna
-            password_n = getpass.getpass('Insert password: ')
+            password_n = getpass('Insert password: ')
             dic[query] = {
                     'username': username_n,
                     'password': password_n
@@ -76,50 +57,38 @@ def search_and_add(query, dic):
     return dic
 
 def log_in(username, password):
-    # deriva il percorso del file associato all'utente
-    path_file = username.encode().hex() + '.cred'
+    path_file = BLAKE2b.new(digest_bits=512, data=username.encode()).hexdigest()
     if os.path.exists(path_file):
         try:
             credentials = load_data(path_file, password)
         except ValueError as err:
             print('Authentication failed')
             return
-        if credentials is not None:
-            prompt = 'Credentials to search:'
-            prompt += '\n(leave blank and press "enter" to save and exit)\n'
-            while True:
-                query = input(prompt)
-                if query != '':
-                    credentials = search_and_add(query, credentials)
-                else:
-                    try:
-                        print('Saving data...')
-                        save_and_exit(path_file, password, credentials)
-                        print('Data saved!')
-                    except IOError:
-                        print('Error while saving, new data has not been updated!')
-                    return
+        except IOError as err:
+            print('Error loading data:')
+            print(err)
+            return
     else:
         prompt = 'User not found. Add as new?'
         prompt += '\n(y to continue, anything else to cancel)\n'
         sign_up = input(prompt)
         if sign_up == 'y':
             credentials = {}
-            prompt = 'Credentials to search:'
-            prompt += '\n(leave blank and press "enter" to save and exit)\n'
-            while True:
-                query = input(prompt)
-                if query != '':
-                    credentials = search_and_add(query, credentials)
-                else:
-                    try:
-                        print('Saving data...')
-                        save_and_exit(path_file, password, credentials)
-                        print('Data saved!')
-                    except IOError:
-                        print('Error while saving, new data has not been updated!')
-                    return
         else:
+            return
+    prompt = 'Credentials to search:'
+    prompt += '\n(leave blank and press "enter" to save and exit)\n'
+    while True:
+        query = input(prompt)
+        if query != '':
+            credentials = search_and_add(query, credentials)
+        else:
+            try:
+                print('Saving data...')
+                save_and_exit(path_file, password, credentials)
+                print('Data saved!')
+            except IOError:
+                print('Error while saving, new data has not been updated!')
             return
 
 #MAIN
@@ -131,6 +100,5 @@ while True:
         print('Goodbye!')
         exit()
     else:
-        # leggi la password in maniera opportuna
-        password = getpass.getpass('Password: ')
+        password = getpass('Password: ')
         log_in(username, password)
